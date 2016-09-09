7314ad3bf76888eacff71ed9bd91de6175129c7f
Andrey Helldar <helldar@ai-rus.com>
2016-09-10 01:32:43 +0900

    Что-то пошло не так...

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,13 +1,13 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
+ * We'll load jQuery and the MaterializeCSS jQuery plugin which provides support
+ * for JavaScript based MaterializeCSS features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
 
 window.$ = window.jQuery = require('jquery');
-require('../../../node_modules/materialize-css/bin/materialize');
+require('../../../node_modules/materialize-css/dist/js/materialize');
 require('./functions');
 
 /**
