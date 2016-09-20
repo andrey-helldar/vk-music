5564c1b024d26ff66e9dd1d6e494a5409c5b63a0
Andrey Helldar <helldar@ai-rus.com>
2016-09-20 14:59:47 +0900

    Добавлена главная страница с плитками

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,1 @@
-@extends('layouts.app')
-
-@section('site_title')
-
-@section('content')
-    <div class="container audio-auto-loading">
-
-        @if(Auth::guest())
-            <vk-auth></vk-auth>
-        @else
-            <audio v-ref:audio></audio>
-        @endif
-
-    </div>
-@endsection
+<?php
