529e55f9d34488bed70b75bc18a96e060bfd8766
Andrey Helldar <helldar@ai-rus.com>
2016-10-10 22:23:37 +0900

    Добавляем вьюху с отражением ошибки авторизации

---
+++ resources/assets/js/components/error.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-cloak>
+        <div>
+            <h3>Errors!</h3>
+
+            <ul class="collection red-text text-darken-2">
+                <li>{{ errorDescription }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                isError:          false,
+                errorDescription: ''
+            }
+        },
+        beforeMount(){
+            this.checkVkErrors();
+        },
+        methods: {
+            checkVkErrors() {
+                if (this.$parent.isError === false) {
+                    window.location.href = '/';
+                    return;
+                }
+
+                this.isError = this.$parent.isError;
+                this.errorDescription = this.$parent.errorDescription;
+            }
+        }
+    }
+</script>
