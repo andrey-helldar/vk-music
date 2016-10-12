fb75f5a374dbfdb556347fe55a1c0e2f47c802df
Andrey Helldar <helldar@ai-rus.com>
2016-10-13 01:16:42 +0900

    Добавлен конфиг линков в футере

---
+++ config/footer-links.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author  Andrey Helldar <helldar@ai-rus.com>
+ * @version 2016-10-13
+ * @since   1.0
+ */
+return [
+    [
+        'title' => 'AI RUS - Professional IT support',
+        'url'   => 'http://ai-rus.com',
+    ],
+];
